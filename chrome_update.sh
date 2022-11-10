1dfe7781109182ee746a1cbab1eee9552547e136
Fabian Gschwenter <fagschwenter@gmail.com>
2022-11-10 13:09:48 +0100

    Add files via upload

---
+++ chrome_update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+apt update
+
+apt upgrade
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+dpkg -i google-chrome-stable_current_amd64.deb
+
+google-chrome
+
+rm -r google-chrome-stable_current_amd64.deb
